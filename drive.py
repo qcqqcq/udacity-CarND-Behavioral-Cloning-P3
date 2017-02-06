85e62d7384b274c9df53fb3baf135d074c6e3c9d
Brandon Quach <qcq@yahoo.com>
2017-02-05 18:37:41 -0800

    Add files needed for submission

--- drive.py
+++ drive.py
@@ -1,6 +1,11 @@
 import os
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 
+# Custom additions
+import pdb
+import PreProcess as pp
+
+
 
 
 import argparse
@@ -38,7 +43,14 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+
+        image_array = pp.preprocess_one_img(image_array)
+
+        transformed_image_array = image_array[None, :, :, :]
+        transformed_image_array = pp.preprocess_batch_img(transformed_image_array)
+
+        steering_angle = float(model.predict(transformed_image_array, batch_size=1))
+
         throttle = 0.2
         print(steering_angle, throttle)
         send_control(steering_angle, throttle)
